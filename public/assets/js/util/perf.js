9077477b12f778a8ce004717a54265982de28670
Todd Gardner <todd@g12solutions.com>
2022-11-11 11:12:27 -0600

    base upgrades

--- public/assets/js/util/perf.js
+++ public/assets/js/util/perf.js
@@ -1,16 +1,13 @@
-(function (ready) {
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-    ready();
-  } else {
-    document.addEventListener("readystatechange", function() {
-      if (document.readyState === "complete") {
-        ready();
-      }
-    });
-  }
-})(function perf() { /* the document is now complete. */
+/**
+ * Custom Real User Monitoring Performance Agent
+ * Demonstration for "Fundamentals of Web Performance"
+ * by Todd Gardner <todd@toddhgardner.com>
+ *
+ * Not for production use.
+ */
+(() => {
 
-  var data = {
+  const payload = {
     url: window.location.href,
     dcl: 0,
     load: 0,
@@ -18,54 +15,80 @@
     lcp: 0,
     cls: 0,
     fid: 0
-  };
+  }
 
-  var fcpObserver = new PerformanceObserver(function handleFCP(entryList) {
-    var entries = entryList.getEntries() || [];
-    entries.forEach(function(entry) {
+  // Navigation Performance Timings
+  onDocumentReady(() => {
+    setTimeout(() => { // "load" isn't done until the next cycle
+      let navEntry = performance.getEntriesByType("navigation")[0];
+      payload.dcl = navEntry.domContentLoadedEventStart;
+      payload.load = navEntry.loadEventStart;
+      console.log('Navigation Performance Timing', navEntry);
+    }, 0);
+  });
+
+  // First Contentful Paint
+  new PerformanceObserver((entryList) => {
+    let entries = entryList.getEntries() || [];
+    entries.forEach((entry) => {
       if (entry.name === "first-contentful-paint") {
-        data.fcp = entry.startTime;
-        console.log("Recorded FCP Performance: " + data.fcp);
+        payload.fcp = entry.startTime;
+        console.log(`FCP: ${payload.fcp}`);
       }
     });
   }).observe({ type: "paint", buffered: true });
 
-  var lcpObserver = new PerformanceObserver(function handleLCP(entryList) {
-    var entries = entryList.getEntries() || [];
-    entries.forEach(function(entry) {
-      if (entry.startTime > data.lcp) {
-        data.lcp = entry.startTime;
-        console.log("Recorded LCP Performance: " + data.lcp);
+  // Largest Contentful Paint
+  new PerformanceObserver((entryList) => {
+    let entries = entryList.getEntries() || [];
+    entries.forEach((entry) => {
+      if (entry.startTime > payload.lcp) {
+        payload.lcp = entry.startTime;
+        console.log(`LCP: ${payload.lcp}`);
       }
     });
   }).observe({ type: "largest-contentful-paint", buffered: true });
 
-  var clsObserver = new PerformanceObserver(function handleCLS(entryList) {
-    var entries = entryList.getEntries() || [];
-    entries.forEach(function(entry) {
+  // Cumulative Layout Shift
+  new PerformanceObserver((entryList) => {
+    let entries = entryList.getEntries() || [];
+    entries.forEach((entry) => {
       if (!entry.hadRecentInput) {
-        data.cls += entry.value;
-        console.log("Increased CLS Performance: " + data.cls);
+        payload.cls += entry.value;
+        console.log(`CLS: ${payload.cls}`);
       }
     });
   }).observe({ type: "layout-shift", buffered: true });
 
-  var fidObserver = new PerformanceObserver(function handleFID(entryList) {
-    var entries = entryList.getEntries() || [];
-    entries.forEach(function(entry) {
-      data.fid = entry.processingStart - entry.startTime;
-      console.log("Recorded FID Performance: " + data.fid);
+  // First Input Delay
+  new PerformanceObserver((entryList) => {
+    let entries = entryList.getEntries() || [];
+    entries.forEach((entry) => {
+      payload.fid = entry.processingStart - entry.startTime;
+      console.log(`FID: ${payload.fid}`);
     });
   }).observe({ type: "first-input", buffered: true });
 
-  window.addEventListener("beforeunload", function() {
-    var navEntry = performance.getEntriesByType("navigation")[0];
-    data.dcl = navEntry.domContentLoadedEventStart;
-    data.load = navEntry.loadEventStart;
 
-    var payload = JSON.stringify(data);
-    navigator.sendBeacon("/api/perf", payload);
-    console.log("Sending performance:", payload);
+  window.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === 'hidden') {
+      let data = JSON.stringify(payload);
+      navigator.sendBeacon("/api/perf", data);
+      console.log("Sending performance:", data);
+    }
   });
 
-});
+})();
+
+
+
+// Utility functions to make example easier to understand.
+function onDocumentReady(onReady) {
+  if (document.readyState === "complete") { onReady(); }
+  else {
+    document.addEventListener('readystatechange', (event) => {
+      if (document.readyState === "complete") { onReady(); }
+    });
+  }
+}
+
